413050383144b7c80c5ce49f54883e7a2c75e027
Aldo Vignone <aldo.vignone@gmail.com>
2020-05-19 20:17:49 -0300

    - Remove old endpoints - Add test endpoint that returns mock JSON - Set express's default path as dist folder

--- src/server/server.js
+++ src/server/server.js
@@ -2,11 +2,9 @@ const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 
-// Setup empty JS object to act as endpoint for all routes
-projectData = {};
-
 // Start up an instance of app
 const app = express();
+app.use(express.static('dist'))
 
 /* Middleware*/
 //Here we are configuring express to use body-parser as middle-ware.
@@ -20,19 +18,13 @@ app.use(cors());
 app.use(express.static('website'));
 
 // Routes
-app.get('/projectData', (req, res) => {
-    console.log('GET projectData');
-    res.send(projectData);
-});
+app.get('/', function (req, res) {
+    res.sendFile('dist/index.html')
+})
 
-app.post('/projectData', (req, res) => {
-    console.log('POST projectData: data = ' + JSON.stringify(req.body));
-    const newData = req.body;
-    projectData.temperature = newData.temperature;
-    projectData.date = newData.date;
-    projectData.userResponse = newData.userResponse;
-    res.status(200).end();
-});
+app.get('/test', function (req, res) {
+    res.send({'mock': 'test'});
+})
 
 // Setup Server
-app.listen(8000, () => (console.log("Running server!")));
+app.listen(8081, () => (console.log("Running server!")));
