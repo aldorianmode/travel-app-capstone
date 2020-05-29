6d829ca2c3bb58b295c2896a6e60647027f937da
Aldo Vignone <aldo.vignone@gmail.com>
2020-05-28 23:53:25 -0300

    Bug fix: await missing while calling getData

--- src/server/js/geonamesAPI.js
+++ src/server/js/geonamesAPI.js
@@ -3,7 +3,7 @@ const { getData } = require('./httpAPI');
 const baseUrl = 'http://api.geonames.org/';
 
 const getDestinationInfo = async (destinationName) => {
-    const result = getData(`${baseUrl}searchJSON?username=${process.env.GEONAMES_USR}&name_startsWith=${destinationName}&maxRows=10`);
+    const result = await getData(`${baseUrl}searchJSON?username=${process.env.GEONAMES_USR}&name_startsWith=${destinationName}&maxRows=10`);
     const firstResult = result.geonames[0];
     return {
         'name': firstResult.name,
