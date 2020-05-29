59c2ce62d3b3de444ae77bcaa31d9f1eeb3daae2
Aldo Vignone <aldo.vignone@gmail.com>
2020-05-28 22:47:01 -0300

    Bug Fix: input parameter wasn't used. Undefined variable was being used

--- src/server/js/geonamesAPI.js
+++ src/server/js/geonamesAPI.js
@@ -3,7 +3,7 @@ import { getData } from './httpAPI';
 const baseUrl = 'http://api.geonames.org/';
 
 const getCityInfo = async (cityName) => {
-    const result = getData(`${baseUrl}searchJSON?username=${process.env.GEONAMES_USR}&name_startsWith=${name}&maxRows=10`);
+    const result = getData(`${baseUrl}searchJSON?username=${process.env.GEONAMES_USR}&name_startsWith=${cityName}&maxRows=10`);
     const firstResult = result.geonames[0];
     return {
         'name': firstResult.name,
