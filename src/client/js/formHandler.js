61aabbbca4f5bc54bb7a1621fc0c8343f04bfc9d
Aldo Vignone <aldo.vignone@gmail.com>
2020-05-30 17:15:59 -0300

    Get destination location and weather info

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -1,11 +1,25 @@
-import { getDestinationData } from './httpAPI';
+import { 
+    getDestinationData,
+    getCurrentWeatherData,
+    getForecastWeatherData,
+} from './httpAPI';
+import { getDaysLeftFromNow } from './utils';
 
 async function handleSubmit(event) {
     event.preventDefault();
 
+    // Get trip destination input
     const destination = document.getElementById('destination').value;
     const destinationInfo = await getDestinationData(destination);
-    console.log(`Destination info received = ${JSON.stringify(destinationInfo)}`);
+
+    // Get trip start date input
+    const inputDate = document.getElementById('date').value;
+    // Calculate days left to start the trip
+    const daysLeft = getDaysLeftFromNow(inputDate);
+    // Get destination's latitude and longitude
+    const { lat, lon } = destinationInfo;
+    // If the trip starts within a week get current weather, if not get forecast (project's letter decision)
+    const weatherInfo = (daysLeft <= 7) ? await getCurrentWeatherData(lat, lon) : await getForecastWeatherData(lat, lon);
 }
 
 export { handleSubmit };
