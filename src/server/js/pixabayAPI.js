9b04e646eee0efc6396cee5672d5ef5cfa45de3f
Aldo Vignone <aldo.vignone@gmail.com>
2020-05-31 13:17:28 -0300

    Bug fix: Accessing wrong key to get image URL

--- src/server/js/pixabayAPI.js
+++ src/server/js/pixabayAPI.js
@@ -4,8 +4,8 @@ const baseUrl = 'https://pixabay.com/api/';
 
 const getImageUrl = async (imageTopic) => {
     const result = await getData(`${baseUrl}?key=${process.env.PIXABAY_API_KEY}&category=places&image_type=photo&q=${imageTopic}`);
-    // If images available, get first one or return undefined 
-    const imageUrl = result.totalHits > 0 ? result.hits[0].imageUrl : undefined;
+    // If images available, get first one or return undefined
+    const imageUrl = result.totalHits > 0 ? result.hits[0].webformatURL : undefined;
     return {
         'imageUrl': imageUrl,
     };
