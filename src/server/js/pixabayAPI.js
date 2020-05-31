deb0621088bd3932e86fdeee06c8deb2b2b6f1e0
Aldo Vignone <aldo.vignone@gmail.com>
2020-05-31 12:55:02 -0300

    Bug fix: Return imageUrl = undefined if no results are found

--- src/server/js/pixabayAPI.js
+++ src/server/js/pixabayAPI.js
@@ -3,13 +3,11 @@ const { getData } = require('./httpAPI');
 const baseUrl = 'https://pixabay.com/api/';
 
 const getImageUrl = async (imageTopic) => {
-    const result = await getData(`${baseUrl}?key=${process.env.PIXABAY_API_KEY}
-        &category=places
-        &image_type=photo
-        &q=${imageTopic}`);
-    const firstResult = result.hits[0];
+    const result = await getData(`${baseUrl}?key=${process.env.PIXABAY_API_KEY}&category=places&image_type=photo&q=${imageTopic}`);
+    // If images available, get first one or return undefined 
+    const imageUrl = result.totalHits > 0 ? result.hits[0].imageUrl : undefined;
     return {
-        'imageUrl': firstResult.imageUrl,
+        'imageUrl': imageUrl,
     };
 };
 
