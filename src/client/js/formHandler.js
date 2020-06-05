d87f502b38aca0b70772fb4bc3ae564f4d253cbf
Aldo Vignone <aldo.vignone@gmail.com>
2020-06-04 22:04:49 -0300

    Refactor formHandler, updating new usage of backend API

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -2,24 +2,50 @@ import {
     getDestinationData,
     getCurrentWeatherData,
     getForecastWeatherData,
+    saveTrip
 } from './httpAPI';
-import { getDaysLeftFromNow } from './utils';
+import { getDaysLeftFromNow, setTripResultHTMLElement } from './utils';
 
 async function handleSubmit(event) {
     event.preventDefault();
 
     // Get trip destination input
     const destination = document.getElementById('destination').value;
-    const destinationInfo = await getDestinationData(destination);
+    // Get trip's start date input
+    const startDate = document.getElementById('startDate').value;
+    // Get trip's end date input
+    const endDate = document.getElementById('endDate').value;
 
-    // Get trip start date input
-    const inputDate = document.getElementById('date').value;
-    // Calculate days left to start the trip
-    const daysLeft = getDaysLeftFromNow(inputDate);
-    // Get destination's latitude and longitude
-    const { lat, lon } = destinationInfo;
-    // If the trip starts within a week get current weather, if not get forecast (project's letter decision)
-    const weatherInfo = (daysLeft <= 7) ? await getCurrentWeatherData(lat, lon) : await getForecastWeatherData(lat, lon);
+    if (!destination && !startDate && !endDate) {
+        //TODO: Set border colors to the empty inputs
+        return;
+    }
+    // TODO: Add input values consistency
+
+    const destinationDataResp = await getDestinationData(destination);
+    if (destinationDataResp.ok) {
+        // Get destination's latitude and longitude
+        // destinationInfo = { name, lat, lon, imageUrl }
+        const destinationInfo = destinationDataResp.data;
+        const { lat, lon } = destinationInfo;
+
+        // Calculate days left to start the trip
+        const daysLeft = getDaysLeftFromNow(startDate);
+
+        // If the trip starts within a week get current weather, if not get forecast (project's letter decision)
+        const weatherDataResp = (daysLeft <= 7) ? await getCurrentWeatherData(lat, lon) : await getForecastWeatherData(lat, lon);
+
+        if (weatherDataResp.ok) {
+            // weatherInfo = { temp, app_temp, description }
+            const weatherInfo = weatherDataResp.data;
+            const tripInfo = { destinationInfo, weatherInfo, startDate, endDate };
+            console.log(`tripInfo = ${JSON.stringify(tripInfo)}`);
+            // Show results
+            setTripResultHTMLElement(tripInfo);
+            // Save trip in backend
+            saveTrip(tripInfo);
+        }        
+    }    
 }
 
 export { handleSubmit };
